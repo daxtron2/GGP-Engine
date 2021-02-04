67ee4274d65163eaa8492515d23c9e3706dedb0e
TJ Wolschon <tjwolschon@gmail.com>
2021-02-04 13:16:45 -0500

    HW1

---
+++ DX11Starter/Mesh.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <d3d11.h>
+#include <wrl/client.h>
+#include "Vertex.h"
+#include <memory>
+
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+
+class Mesh
+{
+private:
+	ComPtr<ID3D11Buffer> vertexBuffer;
+	ComPtr<ID3D11Buffer> indexBuffer;
+	int indexBufferLength = 0;
+
+public:
+	ComPtr<ID3D11Buffer> GetVertexBuffer();
+	ComPtr<ID3D11Buffer> GetIndexBuffer();
+	int GetIndexCount();
+
+	//ctor taking mesh data and d3d device
+	Mesh(Vertex* vertices, int vertexCount, 
+		 unsigned int* indices, int indexCount, 
+		 ComPtr<ID3D11Device> device);
+
+};
+
